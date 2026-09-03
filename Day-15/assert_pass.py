b37fc691e1955796b72c55f7efa0b5a78a66a4c2
gumpulapallavi-sketch <gumpulapallavi@gmail.com>
2026-09-03 15:55:46 +0530

    assert_pass and patterns

---
+++ Day-15/assert_pass.py
@@ -0,0 +1,10 @@
+#keyword - pass, assert
+#pass- whenever you dont what to write inside the code then use the pass keyword.
+#  it is used to define the empty block code
+
+if 10%2==0:
+    pass
+
+#assert- it is used for debugging purpose. assert keyword is used for development purpose.
+# check whether the condition is True or False.
+
---
+++ Day-15/patterns.py
@@ -0,0 +1,57 @@
+'''
+
+for i in  range(10):
+    for j in range(5):
+        print('*',end =' ')
+    print()
+    '''
+'''
+for row in  range(5):
+    for col in range(5):
+        print('*',end =' ')
+    print()
+    '''
+'''
+for row in  range(5):
+    for col in range(5):
+        print(row,end =' ')
+    print()
+'''
+'''
+for row in  range(5):
+    for col in range(5):
+        print(col,end =' ')
+    print()
+    '''
+'''
+for i in  range(5):
+    for j in range(5):
+        print(i+j,end =' ')
+    print()
+    '''
+
+'''
+for i in  range(5):
+    for j in range(5):
+        print(j%2,end =' ')
+    print()
+'''
+'''
+for i in  range(5):
+    for j in range(5):
+        print((i+j)%2,end =' ')
+    print()
+    '''
+'''
+for i in  range(5):
+    for j in range(i+1):
+        print("*",end =' ')
+    print()
+    '''
+'''
+for i in  range(5):
+    for j in range(5-i):
+        print("*",end =' ')
+    print()
+    '''
+
